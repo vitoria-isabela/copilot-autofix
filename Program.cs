01991794867e3e0d55416c95128757ae6424c7b5
Vitória Isabela <VitoriaIsabela.deOliveira@thomsonreuters.com>
2025-08-04 22:57:29 -0300

    Add project.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        string userInput = "../../../etc/passwd";
+
+        string filePath = Path.Combine("/var/www/uploads/", userInput);
+
+        try
+        {
+            string fileContent = File.ReadAllText(filePath);
+            Console.WriteLine("File content: " + fileContent);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+    }
+}
